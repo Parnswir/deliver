2033d090abc72d059f8a0daaa52631eb750137fc
Maximilian Grundke <maximiliangrundke@googlemail.com>
2017-06-10 22:42:51 +0200

    Add MidwifeService to request list of midwives near MindSpace Berlin

---
+++ frontend/src/app/services/MidwifeService.ts
@@ -0,0 +1,36 @@
+import { Injectable }      from '@angular/core';
+import { Http, Response }  from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class MidwifeService {
+  // todo: access location service:
+  private url = 'http://localhost:8080/midwives?lon=52.512334&lat=13.390027';
+
+  constructor (private http: Http) {}
+
+  getMidwives(): Observable<any[]> {
+    return this.http.get(this.url)
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    return res.json() || {};
+  }
+
+  private handleError (error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+}
