37aa6152994b88928bb5fa1f2455fceab862c586
Haitec <me@mrhaitec.de>
2017-06-10 19:34:07 +0200

    added placeholder pages

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { DetailPage } from '../pages/detail/detail';
+import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
+import { MapPage } from '../pages/map/map';
+import { ProfilePage } from '../pages/profile/profile';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -14,9 +16,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    DetailPage,
+    ListPage,
+    LoginPage,
+    MapPage,
+    ProfilePage,
     TabsPage
   ],
   imports: [
@@ -26,9 +30,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    DetailPage,
+    ListPage,
+    LoginPage,
+    MapPage,
+    ProfilePage,
     TabsPage
   ],
   providers: [
