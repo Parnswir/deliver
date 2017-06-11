452865b726d47ac1cc7cbc5b64a0d729d905514b
Maximilian Grundke <maximiliangrundke@googlemail.com>
2017-06-11 05:23:02 +0200

    Add fake map view

--- frontend/src/pages/map/map.ts
+++ frontend/src/pages/map/map.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { MidwifeService } from './../../app/services/MidwifeService';
+
 @Component({
   selector: 'page-map',
-  templateUrl: 'map.html'
+  templateUrl: 'map.html',
+  providers: [MidwifeService]
 })
 export class MapPage {
 
-  constructor(public navCtrl: NavController) {
+  mode = 'Observable'
+  errorMessage:string
+  results:any=[]
 
+  constructor(public navCtrl: NavController, private midwifeService: MidwifeService) {
+
+  }
+
+  ngOnInit() {
+    this.getMidwives();
+  }
+
+  getMidwives() {
+    this.midwifeService.getMidwives()
+                         .subscribe(
+                           results => this.results = results,
+                           error =>  this.errorMessage = <any>error);
   }
 
 }
